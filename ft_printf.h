6f7e66b1525ca6825fc4e9b53880172b2d6fcd9e
seozkan <ozkan003@gmail.com>
2022-12-24 17:31:24 +0300

    v1.7

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: seozkan <seozkan@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 21:24:37 by seozkan           #+#    #+#             */
-/*   Updated: 2022/12/20 21:24:37 by seozkan          ###   ########.fr       */
+/*   Updated: 2022/12/24 17:30:19 by seozkan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,5 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_putchar(int c);
-int	ft_putstr(char *str);
-int	ft_putnbr(unsigned long long nbr, int base, char *str, int *nbr_list);
-int	ft_nbr(unsigned long long nbr, int base, char *str, int mod);
-int	ft_format(va_list args, char format);
 int	ft_printf(const char *str, ...);
 #endif
